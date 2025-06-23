56ea79a54c47c31badcdd4d7cbb2fe4d2b1de3fb
Clémentine Scohy <clembytes@proton.me>
2025-06-23 21:21:00 +0200

    SERVER: comments

--- server.c
+++ server.c
@@ -7,9 +7,13 @@
 #include <string.h>
 
 int main(void) {
-    // A socket is like a case number (a file descriptor)
-    // Here, we tell him that we wanna use the IPV4 protocol family (AF_INET),
-    // and the TCP protocol (SOCK_STREAM)
+    /*
+    Create a socket and get its file descriptor.
+    
+    A socket is like a case number (a file descriptor)
+    Here, we tell him that we wanna use the IPV4 protocol family (AF_INET),
+    and the TCP protocol (SOCK_STREAM)
+    */
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("socket() failed");
@@ -25,6 +29,7 @@ int main(void) {
         return 1;
     }
 
+    // Define IP address and port
     const char *interface = "0.0.0.0";
     struct in_addr mysinaddr;
     ret = inet_aton(interface, &mysinaddr);
@@ -39,19 +44,23 @@ int main(void) {
         .sin_addr = mysinaddr,
     };
 
+    // Attach socket to the previously defined address and port
     ret = bind(sockfd, (struct sockaddr*) &myaddr, sizeof myaddr);
     if (ret < 0) {
         perror("bind() failed");
         return 1;
     }
 
+    // Mark the socket as ready go receive entry connexions
     ret = listen(sockfd, 1);
     if (ret < 0) {
         perror("listen() failed");
         return 1;
     }
 
+    // Repeat indefinitely for each new client
     while (1) {
+        // Get client connexion address
         struct sockaddr_in client_adddr;
         socklen_t client_addr_len = sizeof client_adddr;
         int clientfd = accept(sockfd, (struct sockaddr*) &client_adddr, &client_addr_len);
@@ -60,6 +69,7 @@ int main(void) {
             return 1;
         }
         printf("\n --- NEW CONNEXION RECEIVED, clientfd: %d ---\n", clientfd);
+        // Get client IP address in '0.0.0.0' format for printing
         char dst[16];
         const char* ret2 = inet_ntop(AF_INET, &client_adddr.sin_addr, dst, sizeof dst);
         if (ret2 == NULL) {
@@ -70,7 +80,9 @@ int main(void) {
 
 
         char buf[1000];
+        // Repeat indefinitely for each new request from current client
         while (1) {
+            // Read data sent from client
             // -1 to keep last bit for 0
             ssize_t n = read(clientfd, buf, (sizeof buf) - 1);
             if (n == 0) {
@@ -87,6 +99,7 @@ int main(void) {
             printf("%s\n", buf);
             printf("-------------------------------------\n");
 
+            // Create header for response
             char header[100];
             int w = snprintf(header, sizeof header, "HTTP/1.0 200 OK\r\nContent-Length: %d\r\n\r\n", n);
             if (w < 0) {
@@ -94,6 +107,7 @@ int main(void) {
                 break;
             }
 
+            // Concatenate header and data of response
             char str[w + n + 1];
             ret = snprintf(str, sizeof str, "%s%s", header, buf);
             if (ret < 0) {
@@ -101,6 +115,7 @@ int main(void) {
                 break;
             }
 
+            // Send response to client (write to client file descriptor)
             ret = write(clientfd, str, w + n);
             if (ret < 0) {
                 perror("write() failed");
