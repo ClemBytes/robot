c2a918fc5e023c8f9667ba6f554fb4b1e8ba7755
Clémentine Scohy <clembytes@proton.me>
2025-06-28 10:03:18 +0200

    BASE64: C: check sizes

--- base64.c
+++ base64.c
@@ -5,6 +5,17 @@
 const char charset[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 int base64(const char* data, size_t len_data, char* res, size_t res_len) {
+    int data_nb_bits = len_data*8;
+    int base64_nb_chars = (data_nb_bits + 5) / 6; // ceil(data_nb_bits/6)
+    // int base64_nb_chars = data_nb_bits / 6;
+    // data_nb_bits += (data_nb_bits%6 != 0);   
+    int padding_len = (4 - base64_nb_chars) % 4;
+    int size_needed = base64_nb_chars + padding_len;
+    if (res_len < size_needed) {
+        printf("Insufficient size\n");
+        return size_needed;
+    }
+
     int j = 0;
     int i = 0;
     while (j + 2 < len_data) {
@@ -27,7 +38,7 @@ int base64(const char* data, size_t len_data, char* res, size_t res_len) {
         i += 3;
     }
 
-    int padding_len = (4 - i) % 4;
+    // int padding_len = (4 - i) % 4;
     if (padding_len == 1) {
         res[i + 0] = '=';
         i += 1;
@@ -42,6 +53,11 @@ int base64(const char* data, size_t len_data, char* res, size_t res_len) {
         i += 3;
     }
     res[i] = 0;
+    if (i != size_needed) {
+        printf("i: %d / size_needed: %d\n", i, size_needed);
+        puts("wat");
+        exit(1);
+    }
     printf("%s\n", res);
     return i;
 }
@@ -51,11 +67,27 @@ int base64_str(const char* s, char* res, size_t res_len) {
 }
 
 int main(void) {
-    char res[1000];
-    base64_str("Many hands make light work.", res, sizeof res);
-    base64_str("Man", res, sizeof res);
-    base64_str("Ma", res, sizeof res);
-    base64_str("M", res, sizeof res);
+    char res[37];
+    int n = base64_str("Many hands make light work.", res, sizeof res);
+    if (n >= sizeof res) {
+        printf("%d: Size of res buffer is not enough: %d given and needs %d!\n", __LINE__, sizeof res, n);
+        return 1;
+    }
+    n = base64_str("Man", res, sizeof res);
+    if (n >= sizeof res) {
+        printf("%d: Size of res buffer is not enough: %d given and needs %d!\n", __LINE__, sizeof res, n);
+        return 1;
+    }
+    n = base64_str("Ma", res, sizeof res);
+    if (n >= sizeof res) {
+        printf("%d: Size of res buffer is not enough: %d given and needs %d!\n", __LINE__, sizeof res, n);
+        return 1;
+    }
+    n = base64_str("M", res, sizeof res);
+    if (n >= sizeof res) {
+        printf("%d: Size of res buffer is not enough: %d given and needs %d!\n", __LINE__, sizeof res, n);
+        return 1;
+    }
 }
 
 
