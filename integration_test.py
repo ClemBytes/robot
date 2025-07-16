f45cbe6fae336d94aa2d281f3d8dcaaeff2de806
Clémentine Scohy <clembytes@proton.me>
2025-07-16 21:18:13 +0200

    CI: test CSS file

--- integration_test.py
+++ integration_test.py
@@ -8,14 +8,28 @@ s.connect(("127.0.0.0", 8000))
 
 print("-------------------")
 print("1) Request CSS file")
-print("-------------------")
 # Send request for CSS
 s.sendall(b"GET /data/template.css HTTP/1.1\r\n\r\n")
+with open("data/template.css") as f:
+    css = f.read()
+
+css_expected = f"HTTP/1.0 200 OK\r\nContent-Type: text/css\r\nContent-Length: {len(css)}\r\n\r\n{css}"
 
 # Receive answer
 data = s.recv(10000)
 
-print(data.decode())
+if data.decode() == css_expected:
+    print("OK for CSS request")
+else:
+    print("ERROR for CSS request:")
+    print(f"Expected:")
+    print("----")
+    print(css_expected)
+    print("----")
+    print(f"Received:")
+    print("----")
+    print(data.decode())
+    print("----")
 
 print("-------------------")
 
@@ -29,7 +43,7 @@ s.sendall(b"GET /data/robot.png HTTP/1.1\r\n\r\n")
 # Receive answer
 data = s.recv(10000)
 
-print(data)
+# print(data)
 print("-------------------")
 
 s.close()
