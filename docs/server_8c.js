68efd5b386af97dfed7b3bc75aa48d77d9d0a18e
Clémentine Scohy <clembytes@proton.me>
2025-07-26 22:55:53 +0200

    DOXYGEN: maj

--- docs/server_8c.js
+++ docs/server_8c.js
@@ -1,5 +1,6 @@
 var server_8c =
 [
+    [ "generate_content", "server_8c.html#a6c727c202469ebd51b620869bdeba04a", null ],
     [ "handle_client", "server_8c.html#a679b3c257edc78213596828b027b05d6", null ],
     [ "main", "server_8c.html#a840291bc02cba5474a4cb46a9b9566fe", null ],
     [ "read_client", "server_8c.html#a6389b664807b5c931e37dfb29b9535be", null ]
